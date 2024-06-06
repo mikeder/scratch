8bd7738270ba63983884dde4bc002b272fa4808b
mikeder <mikedernet@gmail.com>
2024-06-06 11:51:13 -0400

    update publish script

--- scripts/publish_to_ghpages.sh
+++ scripts/publish_to_ghpages.sh
@@ -21,8 +21,9 @@ rm -rf public/*
 echo "Generating Pages"
 cd glitch && make
 
-# echo "Updating gh-pages branch"
-# cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+echo "Updating gh-pages branch"
+cp -R ./server/assets ../../public/
+cd ../../public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-# echo "Pushing to github"
-# git push --all
+echo "Pushing to github"
+git push --all
