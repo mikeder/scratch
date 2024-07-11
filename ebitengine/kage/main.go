16199dfb37e62ee4ee9975de5361704c4b09baf2
mikeder <mikedernet@gmail.com>
2024-07-11 09:55:32 -0400

    reduce gc?

--- ebitengine/kage/main.go
+++ ebitengine/kage/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	_ "embed"
+	"flag"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -28,18 +33,54 @@ var starsShader []byte
 var waterShader []byte
 
 func main() {
+	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+	var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		go func() {
+			if err := pprof.StartCPUProfile(f); err != nil {
+				log.Fatal("could not start CPU profile: ", err)
+			}
+		}()
+		defer pprof.StopCPUProfile()
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
+
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
 	// compile the shader(s)
 	sun, err := ebiten.NewShader(sunShader)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	stars, err := ebiten.NewShader(starsShader)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	water, err := ebiten.NewShader(waterShader)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	// create game struct
@@ -52,8 +93,9 @@ func main() {
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	err = ebiten.RunGame(game)
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
 
 // Struct implementing the ebiten.Game interface.
@@ -102,8 +144,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawRectShader(w, h, g.sun, op)
 
 	// capture current screen
-	cap := ebiten.NewImageFromImage(screen)
-	op.Images[0] = cap
+	op.Images[0] = ebiten.NewImageFromImage(screen)
 
 	// draw water
 	screen.DrawRectShader(w, h, g.water, op)
